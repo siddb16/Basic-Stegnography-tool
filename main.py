ee6fe8946e3e36a4e59826b062844ede91e7e1c5
Siddhant <siddb16@example.com>
2025-09-12 11:34:41 +0000

    d

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Python Project for Encoding and decoding a message/text/secret in a Image Using LSB method
 from PIL import Image
 txt=input("Enter your txt: ")
-binary=bin(txt)
-print(binary)
+bin = ''.join(format(ord(char), '08b') for char in txt)
+print(bin)
